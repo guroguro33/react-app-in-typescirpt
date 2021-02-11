4bb8124f89e2fb14a8698a3f00fcca6772e9213a
toru <t02f447f@gmail.com>
2021-02-12 06:00:35 +0900

    IMPROVE src/Counter.tsx

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -20,6 +20,16 @@ const Counter: React.FC<{}> = () => {
   useEffect(() => {
     renderTimes.current += 1;
   })
+  const ref = useRef<HTMLInputElement>(null!);
+  const focusInput = () => {
+    // const current = ref.current;
+    // if (current != null) current.focus();
+
+    // オプショナルチェインを使った場合
+    // ref.current?.focus();
+
+    ref.current.focus();
+  }
 
   return (
     <div>
@@ -27,6 +37,8 @@ const Counter: React.FC<{}> = () => {
       <button onClick={ increment }>+1</button>
       <button onClick={ decrement }>-1</button>
       <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type="text"/>
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
     
